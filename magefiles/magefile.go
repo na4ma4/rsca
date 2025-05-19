de658db19ea7e1b42d11b104f8c20a9dc158db6d
na4ma4 <na4ma4@users.noreply.github.com>
2025-05-19 11:08:04 +1000

    fix: updated magefiles to use generic Test target

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -13,11 +13,7 @@ import (
 
 // TestLocal update, protoc, format, tidy, lint & test.
 func TestLocal(ctx context.Context) {
-	// mg.CtxDeps(ctx, mage.CFSSL.Generate)
-	mg.CtxDeps(ctx, mage.Protobuf.GenGo)
-	mg.CtxDeps(ctx, mage.Protobuf.GenGoGRPC)
-	mg.CtxDeps(ctx, mage.Golang.Lint)
-	mg.CtxDeps(ctx, mage.Golang.Test)
+	mg.CtxDeps(ctx, mage.Test)
 }
 
 var Default = TestLocal
